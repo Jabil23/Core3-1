54381735879b3dc8d35dc8357154b8c8b0e3104f
Cyvster <41338761+Cyvster@users.noreply.github.com>
2018-07-25 20:36:59 -0500

    Update forceLightningCone2.lua
    4x damage

--- MMOCoreORB/bin/scripts/commands/forceLightningCone2.lua
+++ MMOCoreORB/bin/scripts/commands/forceLightningCone2.lua
@@ -44,8 +44,8 @@
 ForceLightningCone2Command = {
 	name = "forcelightningcone2",
 
-	minDamage = 600,
-	maxDamage = 1200,
+	minDamage = 2400,
+	maxDamage = 4800,
 	speed = 4.0,
 	forceCost = 250,
 	accuracySkillMod = "forcelightning_accuracy",
