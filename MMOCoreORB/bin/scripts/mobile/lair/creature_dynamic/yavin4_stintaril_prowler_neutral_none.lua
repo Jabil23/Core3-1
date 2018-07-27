198d60691644e2972018d3ac7d7b5776344b133b
Cyvster <41338761+Cyvster@users.noreply.github.com>
2018-07-27 16:41:35 -0500

    Update yavin4_stintaril_prowler_neutral_none.lua
    Increase spawn limit from 5 to 15, not sure what effect it will have. I'm hoping it increases the number spawned.

--- MMOCoreORB/bin/scripts/mobile/lair/creature_dynamic/yavin4_stintaril_prowler_neutral_none.lua
+++ MMOCoreORB/bin/scripts/mobile/lair/creature_dynamic/yavin4_stintaril_prowler_neutral_none.lua
@@ -1,6 +1,6 @@
 yavin4_stintaril_prowler_neutral_none = Lair:new {
 	mobiles = {{"stintaril_prowler",1}},
-	spawnLimit = 5,
+	spawnLimit = 25,
 	buildingsVeryEasy = {},
 	buildingsEasy = {},
 	buildingsMedium = {},
