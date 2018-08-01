189895b251c5b2f88354ab9d7398f0902b406613
Cyvster <41338761+Cyvster@users.noreply.github.com>
2018-07-31 23:41:07 -0500

    Update ForceRun2Command.h
    changed divisor to 1 to remove debuff

--- MMOCoreORB/src/server/zone/objects/creature/commands/ForceRun2Command.h
+++ MMOCoreORB/src/server/zone/objects/creature/commands/ForceRun2Command.h
@@ -45,7 +45,7 @@ public:
 
 		Locker locker(multBuff);
 
-		multBuff->setSkillModifier("private_damage_divisor", 20);
+		multBuff->setSkillModifier("private_damage_divisor", 1); //changed divisor to 1 to remove debuff
 
 		creature->addBuff(multBuff);
 
